f2be3fcc50c399b8099188b41f89ad1567a236a8
Ayodeji-Dasylva <68438024+Developers-College@users.noreply.github.com>
2021-03-26 06:51:32 +0100

    Polling Service Executable Main Project

---
+++ polling-service-main/src/main/java/com/kry/servicepoller/PollingServiceMain.java
@@ -0,0 +1,12 @@
+package com.kry.servicepoller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingServiceMain {
+    public static void main(String[] args) {
+        SpringApplication.run(PollingServiceMain.class, args);
+    }
+
+}
